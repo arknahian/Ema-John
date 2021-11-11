88bbf808decc964d6314b65445d3e9fd9c80d5f9
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2021-11-11 21:18:03 +0600

    n

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import './Header.css';
+import { userContext } from "./../../App";
 
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(userContext);
     return (
         <div className="header">
             <img src={logo} alt="" />
              <nav><Link to="/shop">Shop</Link>
              <Link to="/review">Review</Link>
              <Link to="/inventory">Manage Inventory</Link>
+             <button onClick={() => setLoggedInUser({})}>Sign Out</button>
              </nav>
         </div>
     );
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>Not Found 404</h1>
+            <h1>This page not NotFound</h1>
         </div>
     );
 };
